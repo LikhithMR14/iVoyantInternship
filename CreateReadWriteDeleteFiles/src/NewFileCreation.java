0dc62b62a0ddb69fa59a076bc6cc3ef4dee2843c
LikhithMR14 <likhiths@ivoyant.com>
2024-08-19 14:22:22 +0530

    Added Creasting,Reading,Writing and Deleting files

---
+++ CreateReadWriteDeleteFiles/src/NewFileCreation.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class NewFileCreation {
+    public static void main(String[] args) throws IOException {
+//      Creating a file:-
+//        File newFile=new File("newFileCreation.txt");
+//        newFile.createNewFile();
+
+//      Writing in the file:-
+//        FileWriter fileWriter=new FileWriter("newFileCreation.txt");
+//        fileWriter.write("This is my first time using FileWriter\nHello! I am Likhith Siddharth M R.");
+//        fileWriter.close();
+
+//      Reading from the file:-
+//        File myFile=new File("newFileCreation.txt");
+//        Scanner sc=new Scanner(myFile);
+//        while(sc.hasNextLine()) {
+//            System.out.println(sc.nextLine());
+//        }
+//        sc.close();
+
+//      Deleting a file:-
+//        File myFile=new File("newFileCreation.txt");
+//        if(myFile.delete())
+//            System.out.println("The file newFileCreation.txt is been deleted successfully");
+//        else
+//            System.out.println("There was a problem while deleting the mentioned file");
+
+    }
+}
